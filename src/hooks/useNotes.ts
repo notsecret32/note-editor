5459576338ec40b6a89a3fd485327f71f83c78bc
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2023-12-05 01:48:02 +0300

    feat: added a hook for working with notes

---
+++ src/hooks/useNotes.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react'
+import { INote } from 'types/note.type'
+import { getAllNotes } from 'utils/db.utils'
+
+export function useNotes() {
+	const [notes, setNotes] = useState<INote[]>([])
+
+	useEffect(() => {
+		async function fetchData() {
+			const notesData = await getAllNotes()
+			setNotes(notesData)
+		}
+
+		fetchData()
+	}, [notes])
+
+	return notes
+}

6f89793d2522d731682f6f49cfbc779d4699902a
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2023-12-06 00:03:59 +0300

    feat: added improved ability to import components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import { Note } from './Note'
+import { NoteList } from './NoteList'
+import { NoteModal } from './NoteModal'
+import { TagsSearch } from './TagsSearch'
+
+export { Note, NoteList, NoteModal, TagsSearch }

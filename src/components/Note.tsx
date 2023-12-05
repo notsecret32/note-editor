4077677232402d2b4487bde5c2a9004b40a654c3
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2023-12-06 02:30:19 +0300

    refactor: its own implementation of the modal window has been completely removed
    Unnecessary overcomplication of the code, it is better to make modal windows based on existing components and not reinvent the bike.

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -5,23 +5,19 @@ import {
   CardContent,
   Typography
 } from '@mui/material'
-import { useModal } from 'hooks/useModal'
 import React, { FC } from 'react'
 import { useDispatch } from 'react-redux'
 import { deleteNote } from 'store/noteReducer'
 import { INote } from 'types/note.type'
-import { NoteModal } from './NoteModal'
 
 export const Note: FC<INote> = ({ id, title, description, tags }) => {
-  const modal = useModal()
-
-  modal.options = {
-    title: 'Редактирование',
-    cancelButtonText: 'Отменить',
-    submitButtonText: 'Сохранить'
-  }
-
   const dispatch = useDispatch()
+
+  const handleEditNote = (event: React.SyntheticEvent) => {
+    event.preventDefault()
+
+    console.log('start edit')
+  }
 
   const handleDeleteNote = (event: React.SyntheticEvent) => {
     event.preventDefault()
@@ -40,7 +36,9 @@ export const Note: FC<INote> = ({ id, title, description, tags }) => {
         <Typography variant="body2">{description}</Typography>
       </CardContent>
       <CardActions>
-        <NoteModal modalType="confirm" buttonTitle="Редактировать" {...modal} />
+        <Button size="small" onClick={handleEditNote}>
+          Редактировать
+        </Button>
         <Button size="small" onClick={handleDeleteNote}>
           Удалить
         </Button>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 import { Note } from './Note'
 import { NoteList } from './NoteList'
-import { NoteModal } from './NoteModal'
 import { TagsSearch } from './TagsSearch'
 
-export { Note, NoteList, NoteModal, TagsSearch }
+export { Note, NoteList, TagsSearch }

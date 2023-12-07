b528b0faa651e2ed106fb00fe3befd66cfe2da92
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2023-12-07 07:05:19 +0300

    feat: the appearance of the `Home` page has been improved

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,59 @@
-import { Container } from '@mui/material'
-import { NoteList, TagsSearch } from 'components'
-import React from 'react'
+import AddIcon from '@mui/icons-material/Add'
+import { Container, Grid, IconButton, Typography } from '@mui/material'
+import { AddNoteModal, NoteList, TagsSearch } from 'components'
+import { FC, useState } from 'react'
 
-export const Home: React.FC = () => {
+export const Home: FC = () => {
+  const [isAddNoteModalOpen, setIsAddNoteModalOpen] = useState(false)
+
+  const handleAddNoteModalOpen = () => {
+    setIsAddNoteModalOpen(true)
+  }
+
+  const handleAddNoteModalClose = () => {
+    setIsAddNoteModalOpen(false)
+  }
+
   return (
-    <Container>
-      <h1>Заметки</h1>
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <TagsSearch tags={['важно', 'допятницы']} />
-      </div>
-      <NoteList />
-    </Container>
+    <>
+      <Container>
+        <Grid container justifyContent="center">
+          <Grid item>
+            <Typography
+              variant="h2"
+              component="h1"
+              align="center"
+              sx={{
+                fontWeight: '700'
+              }}
+            >
+              Заметки
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              margin: '20px 0'
+            }}
+          >
+            <TagsSearch />
+            <IconButton onClick={handleAddNoteModalOpen}>
+              <AddIcon />
+            </IconButton>
+          </Grid>
+          <Grid item>
+            <NoteList />
+          </Grid>
+        </Grid>
+      </Container>
+      <AddNoteModal
+        isOpen={isAddNoteModalOpen}
+        onClose={handleAddNoteModalClose}
+      />
+    </>
   )
 }

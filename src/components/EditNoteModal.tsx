0e9dbe979871a7cb1edbd3de9926d67540167ec4
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2023-12-07 07:02:01 +0300

    feat: added a new `EditNoteModal` that edits the note

---
+++ src/components/EditNoteModal.tsx
@@ -0,0 +1,90 @@
+// EditNoteModal.tsx
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField
+} from '@mui/material'
+import { FC, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { updateNote } from 'store/noteReducer'
+import { extractTags } from 'utils/tags.utils'
+
+interface EditNoteModalProps {
+  isOpen: boolean
+  onClose: () => void
+  noteId: string
+  noteTitle: string
+  noteTitleWithTags: string
+  noteDescription: string
+}
+
+export const EditNoteModal: FC<EditNoteModalProps> = ({
+  isOpen,
+  onClose,
+  noteId,
+  noteTitle,
+  noteTitleWithTags,
+  noteDescription
+}) => {
+  const [noteEditedTitle, setNoteEditedTitle] = useState(noteTitleWithTags)
+  const [noteEditedDescription, setNoteEditedDescription] =
+    useState(noteDescription)
+  const dispatch = useDispatch()
+
+  const handleEditNote = () => {
+    const { tags, sentence } = extractTags(noteEditedTitle || noteTitleWithTags)
+
+    dispatch(
+      updateNote({
+        id: noteId,
+        title: sentence,
+        titleWithTags: noteTitleWithTags,
+        description: noteEditedDescription,
+        tags
+      })
+    )
+
+    onClose()
+  }
+
+  return (
+    <Dialog open={isOpen} onClose={onClose}>
+      <DialogTitle>Редактировать заметку | {noteTitle}</DialogTitle>
+      <DialogContent>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="noteTitle"
+          label="Заголовок"
+          type="text"
+          fullWidth
+          value={noteEditedTitle}
+          onChange={(e) => setNoteEditedTitle(e.target.value)}
+          required
+        />
+        <TextField
+          margin="dense"
+          id="noteDescription"
+          label="Описание"
+          type="text"
+          fullWidth
+          value={noteEditedDescription}
+          onChange={(e) => setNoteEditedDescription(e.target.value)}
+          rows={5}
+          multiline
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose} color="primary">
+          Отменить
+        </Button>
+        <Button onClick={handleEditNote} color="primary">
+          Сохранить
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}

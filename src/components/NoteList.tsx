773040167b95028f3d84f1248448851fc93ed27b
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2023-12-07 18:27:48 +0300

    feat: added a tag filter

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -1,23 +1,35 @@
-import { List } from '@mui/material'
+import { List, Typography } from '@mui/material'
 import { useNotes } from 'hooks/useNotes'
-import React from 'react'
+import React, { useMemo } from 'react'
+import { INote } from 'types/note.type'
 import { Note } from './Note'
 
+const NoteListItem: React.FC<{ note: INote }> = ({ note }) => (
+  <Note
+    key={note.id}
+    id={note.id}
+    title={note.title}
+    titleWithTags={note.titleWithTags}
+    description={note.description}
+    tags={note.tags}
+  />
+)
+
 export const NoteList: React.FC = () => {
-  const notes = useNotes()
+  const { allNotes, selectedNotes } = useNotes()
+
+  const noteListItems = useMemo(() => {
+    const notes = selectedNotes.length === 0 ? allNotes : selectedNotes
+    return notes.map((note) => <NoteListItem key={note.id} note={note} />)
+  }, [allNotes, selectedNotes])
 
   return (
     <List>
-      {notes.map((note) => (
-        <Note
-          key={note.id}
-          id={note.id}
-          title={note.title}
-          titleWithTags={note.titleWithTags}
-          description={note.description}
-          tags={note.tags}
-        />
-      ))}
+      {noteListItems.length > 0 ? (
+        noteListItems
+      ) : (
+        <Typography>Нет заметок.</Typography>
+      )}
     </List>
   )
 }
--- src/components/TagsSearch.tsx
+++ src/components/TagsSearch.tsx
@@ -1,15 +1,23 @@
-import Autocomplete from '@mui/material/Autocomplete'
-import TextField from '@mui/material/TextField'
+import { Autocomplete, TextField } from '@mui/material'
 import { useTags } from 'hooks/useTags'
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { updateTags } from 'store/tagsReducer'
 
 export const TagsSearch: React.FC = () => {
   const { tags } = useTags()
+  const dispatch = useDispatch()
+
+  const handleChange = (e: React.SyntheticEvent, selectedTags: string[]) => {
+    e.preventDefault()
+    dispatch(updateTags(selectedTags))
+  }
 
   return (
     <Autocomplete
       multiple
       options={tags}
+      onChange={handleChange}
       renderInput={(params) => (
         <TextField
           {...params}

4bb1107f05865b7e4abe3642ff343362d915a2b6
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2023-12-07 18:26:10 +0300

    feat: added the ability to receive notes by tags

--- src/hooks/useNotes.ts
+++ src/hooks/useNotes.ts
@@ -1,18 +1,31 @@
 import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { RootState } from 'store/store'
 import { INote } from 'types/note.type'
-import { getAllNotes } from 'utils/db.utils'
+import { getAllNotes, getAllNotesByTags } from 'utils/db.utils'
 
-export function useNotes() {
-  const [notes, setNotes] = useState<INote[]>([])
+export const useNotes = () => {
+  const [allNotes, setAllNotes] = useState<INote[]>([])
+  const [selectedNotes, setSelectedNotes] = useState<INote[]>([])
+  const selector = useSelector((state: RootState) => state.tags.tags)
 
   useEffect(() => {
-    async function fetchData() {
-      const notesData = await getAllNotes()
-      setNotes(notesData)
+    const loadNotes = async () => {
+      const notes = await getAllNotes()
+      setAllNotes(notes)
     }
 
-    fetchData()
-  }, [notes])
+    loadNotes()
+  }, [])
 
-  return notes
+  useEffect(() => {
+    const loadSelectedNotes = async () => {
+      const fetchedSelectedNotes = await getAllNotesByTags(selector)
+      setSelectedNotes(fetchedSelectedNotes)
+    }
+
+    loadSelectedNotes()
+  }, [selector])
+
+  return { allNotes, selectedNotes }
 }
